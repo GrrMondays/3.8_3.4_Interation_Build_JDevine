42f732ddb0187f15831094851324691748784389
GrrMondays <88806696+GrrMondays@users.noreply.github.com>
2022-09-16 11:30:13 +1200

    Add files via upload

---
+++ profile[1].php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head> <!-- For linking my css file, meta data, website title and icon-->
+		<title> Among Music | Profile </title>
+		<meta charset="UTF-8">
+		<meta name="description" content="Graemme's databases that holds music data that him and his 
+		close circle of family and friends are able to login into.">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="keywords" content="Graemme, Electronic Music, Among Music">
+		<meta name="author" content="Johno Devine">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="stylesheet.css"/>
+	</head>
+	<body>
+		<div class="rightbackground"></div>
+		<div id="myNav" class="overlay">
+		  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><span style="color: #ff244f;">&times;</span></a>
+		  <div class="overlay-content">
+			<a href="index.php">Home</a>
+			<a href="music.php">Music</a>
+			<a href="contact.php">Contact</a>
+		  </div>
+		</div>
+		<span style="font-size: 60px;cursor:pointer" onclick="openNav()">&#9776;</span>
+
+			<div class="profilecontent">
+				<div class="section1">
+					
+					
+				</div>
+				<div class="section2">
+					
+					
+				</div>
+				<div class="section3">
+				</div>
+		</div>
+		<h3> Footer </h3>
+		<script src="main.js"></script>
+	</body>
+</html>
+
